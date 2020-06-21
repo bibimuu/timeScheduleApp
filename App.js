c3d0da67b9bba3daff82123bf81493ac2eef351c
平松大悟朗 <h.dai56@gmail.com>
2020-06-22 00:58:22 +0900

    implement fonts

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {useScreens} from "react-native-screens"
+import {AppLoading} from "expo";
+import * as Font from "expo-font";
+
+import scheduleMainNavigator from "./src/navigation/scheduleNavigators";
+import {ScheduleScreen} from "./src/screens/ScheduleScreen";
+
+useScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans" : require("./assets/Fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold" : require("./assets/Fonts/OpenSans-Bold.ttf")
+  });
+}
 
 export default function App() {
+  const [fontLoaded,setFontLoaded] = useState(false);
+  if(!fontLoaded){
+    return (
+    <AppLoading 
+    startAsync={fetchFonts} 
+    onFinish={()=>setFontLoaded(true)}
+    style={styles.container}
+    />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.bold}>kokokoko</Text>
+      <ScheduleScreen/>
     </View>
   );
 }
@@ -14,6 +41,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  bold: {
+    fontFamily: "open-sans-bold"
+  }
 });
---
+++ src/screens/UserScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const UserScreen = props => {
+  return (
+    <View>
+      <Text>aaabbbb</Text>
+    </View>
+  )
+};
+
+export {UserScreen};
